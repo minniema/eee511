45642a43dab949c02856dc8540961e0e15767183
maminyang <maminyang@hotmail.com>
2018-11-04 18:58:37 -0700

    adding changes

--- project4.m
+++ project4.m
@@ -5,15 +5,15 @@ dataPTO=load('dataset_PTO');
 
 %% split into 70 % training and 30% test
 
-trainDC=dataDC.DC(1:round(length(dataDC.DC)*.7),:);
-trainCatDC=CatDC.Category(1:round(length(dataDC.DC)*.7),:);
-testDC=dataDC.DC(round(length(dataDC.DC)*.7)+1:end,:);
-testCatDC=CatDC.Category(round(length(dataDC.DC)*.7)+1:end,:);
+trainDC=dataDC.DC(1:round(length(dataDC.DC)*.8),:);
+trainCatDC=CatDC.Category(1:round(length(dataDC.DC)*.8),:);
+testDC=dataDC.DC(round(length(dataDC.DC)*.8)+1:end,:);
+testCatDC=CatDC.Category(round(length(dataDC.DC)*.8)+1:end,:);
 
-trainPTO=dataPTO.PTO(1:round(length(dataPTO.PTO)*.7),:);
-trainCatPTO=CatPTO.Category(1:round(length(dataPTO.PTO)*.7),:);
-testPTO=dataPTO.PTO(round(length(dataPTO.PTO)*.7)+1:end,:);
-testCatPTO=CatDC.Category(round(length(dataPTO.PTO)*.7)+1:end,:);
+trainPTO=dataPTO.PTO(1:round(length(dataPTO.PTO)*.8),:);
+trainCatPTO=CatPTO.Category(1:round(length(dataPTO.PTO)*.8),:);
+testPTO=dataPTO.PTO(round(length(dataPTO.PTO)*.8)+1:end,:);
+testCatPTO=CatDC.Category(round(length(dataPTO.PTO)*.8)+1:end,:);
 
 
 %% scaling
@@ -35,11 +35,11 @@ net1 = feedforwardnet(hiddensize);
 net1=train(net1,trainDC',trainCatDC');
 %ynet=sim(net1,);
 ynetn=sim(net1,testDC');
-ynetn(ynetn>0.5)=1;
-ynetn(ynetn<=0.5)=0;
+ynetn(ynetn>0.7)=1;
+ynetn(ynetn<=0.7)=0;
 perf1 = mse(net1,testDC',testCatDC');
 
-testrounded=round(ynetn);
+%testrounded=round(ynetn);
 
 %confusion matrix generaiton for nn
 confusionsNN1 = confusionmat(testCatDC, ynetn);
@@ -52,7 +52,7 @@ prec1= tp1 / (tp1 + fp1);
 rec1= tp1 / (tp1 + fn1);
 f1 = (2 * prec1 * rec1) / (prec1 + rec1);
 %calculating roc for curve, positive class define as 1 which is eating
-[X, Y, ~, bayesAUC] = perfcurve(testCatDC, testrounded, '1');
+[X, Y, ~, AUC] = perfcurve(testCatDC, ynetn, '1');
 figure
 plot(X,Y)
     xlabel('False positive rate') 
@@ -66,8 +66,8 @@ net2=train(net2,trainPTO',trainCatPTO');
 %ynet=sim(net1,);
 ynetn2=sim(net2,testPTO');
 % select 0.5 as threshold
-ynetn2(ynetn2>0.5)=1;
-ynetn2(ynetn2<=0.5)=0;
+ynetn2(ynetn2>0.6)=1;
+ynetn2(ynetn2<=0.6)=0;
 
 perf2 = mse(net2,testPTO',testCatPTO');
 
@@ -98,44 +98,61 @@ title('ROC for Classification NN for PTO')
 % xlabel("Scaled Time Index ")
 % ylabel("Scaled Output ")
 % legend('Original','Train Output','30 Point Estimation')
-
-%% train SVM1 for DC
-svm= fitcsvm(trainDC, trainCatDC);
-[DCsvm, svmscore] =  predict(svm, testDC);
-perfsvm1=immse(DCsvm, testCatDC);
-
-confusionsvm = confusionmat(testCatDC, DCsvm);
-tp3=confusionsvm(1, 1);
-fp3=confusionsvm(1, 2);
-fn3=confusionsvm(2, 1);
-tn3=confusionsvm(2, 2);
-%calculating Prec,Recall,F scores
-prec3= tp3 / (tp3 + fp3);
-rec3= tp3 / (tp3 + fn3);
-f3 = (2 * prec3 * rec3) / (prec3 + rec3);
-%calculating roc for curve, positive class define as 1 which is eating
-[X, Y, ~, svmAUC] = perfcurve(truthClass, svmscore(:, 2), '1');
-figure
-plot(X,Y)
-xlabel('False positive rate') 
-ylabel('True positive rate')
-title('ROC for Classification by Logistic Regression SVM')
-
-%% train SVM for PTO
-svm2= fitcsvm(trainPTO, trainCatPTO);
-[ptosvm, svmscore2] =  predict(svm2, testPTO);
-perfsvm2=immse(ptosvm, testCatPTO);
-
-
-%% train using gaussian kernal
-
-svm3= fitcsvm(trainDC, trainCatDC,'KernelFunction','rbf',...
-    'BoxConstraint',Inf);
-[DCsvm3, svmscore3] =  predict(svm3, testDC);
-perfsvm3=immse(DCsvm3, testCatDC);
-
-svm4= fitcsvm(trainPTO, trainCatPTO,'KernelFunction','rbf',...
-'BoxConstraint',Inf);
-[PTOsvm4, svmscore4] =  predict(svm4, testPTO);
-perfsvm4=immse(PTOsvm4, testCatPTO);
+% 
+% %% train SVM1 for DC
+% svm= fitcsvm(trainDC, trainCatDC);
+% [DCsvm, svmscore] =  predict(svm, testDC);
+% perfsvm1=immse(DCsvm, testCatDC);
+% 
+% confusionsvm = confusionmat(testCatDC, DCsvm);
+% tp3=confusionsvm(1, 1);
+% fp3=confusionsvm(1, 2);
+% fn3=confusionsvm(2, 1);
+% tn3=confusionsvm(2, 2);
+% %calculating Prec,Recall,F scores
+% prec3= tp3 / (tp3 + fp3);
+% rec3= tp3 / (tp3 + fn3);
+% f3 = (2 * prec3 * rec3) / (prec3 + rec3);
+% %calculating roc for curve, positive class define as 1 which is eating
+% [X, Y, ~, svmAUC] = perfcurve(testCatDC, svmscore(:, 2), '1');
+% figure
+% plot(X,Y)
+% xlabel('False positive rate') 
+% ylabel('True positive rate')
+% title('ROC for Classification by DC SVM')
+% 
+% %% train SVM for PTO
+% svm2= fitcsvm(trainPTO, trainCatPTO);
+% [ptosvm, svmscore2] =  predict(svm2, testPTO);
+% perfsvm2=immse(ptosvm, testCatPTO);
+% 
+% confusionsvm2 = confusionmat(testCatPTO, PTOsvm);
+% tp2=confusionsvm2(1, 1);
+% fp2=confusionsvm2(1, 2);
+% fn2=confusionsvm2(2, 1);
+% tn2=confusionsvm2(2, 2);
+% %calculating Prec,Recall,F scores
+% prec2= tp2 / (tp2 + fp2);
+% rec2= tp2 / (tp2 + fn2);
+% f2 = (2 * prec2 * rec2) / (prec2 + rec2);
+% %calculating roc for curve, positive class define as 1 which is eating
+% [X, Y, ~, svmAUC] = perfcurve(testCatDC, svmscore(:, 2), '1');
+% figure
+% plot(X,Y)
+% xlabel('False positive rate') 
+% ylabel('True positive rate')
+% title('ROC for Classification by DC SVM')
+% 
+% 
+% %% train using gaussian kernal
+% 
+% svm3= fitcsvm(trainDC, trainCatDC,'KernelFunction','rbf',...
+%     'BoxConstraint',Inf);
+% [DCsvm3, svmscore3] =  predict(svm3, testDC);
+% perfsvm3=immse(DCsvm3, testCatDC);
+% 
+% svm4= fitcsvm(trainPTO, trainCatPTO,'KernelFunction','rbf',...
+% 'BoxConstraint',Inf);
+% [PTOsvm4, svmscore4] =  predict(svm4, testPTO);
+% perfsvm4=immse(PTOsvm4, testCatPTO);
 
